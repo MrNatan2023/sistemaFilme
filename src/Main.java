4f71216692ff6c4a7a86b31adc09971aacb15b7d
NatanFreitas <115927951+MrNatan2023@users.noreply.github.com>
2024-06-12 17:06:16 -0300

    diretor feito

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,33 @@
 import db.DB;
-import entities.Filme;
-import entities.dao.FilmeDao;
-import jdbc.FilmeDaoJDBC;
+import entities.Diretor;
+import jdbc.DiretorDaoJDBC;
+
 
 import java.sql.*;
-import java.text.ParseException;
 import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
         Connection conn = DB.getConnection();
-        FilmeDaoJDBC filmeDao = new FilmeDaoJDBC(conn);
+        DiretorDaoJDBC diretorDao = new DiretorDaoJDBC(conn);
 
 
         try {
             conn.setAutoCommit(false);
             long inicio = System.currentTimeMillis();
 
-            for (int i = 0; i < 10000; i++) {
-                Filme filme = new Filme("Filme " + i, 120, 1996);
-                filmeDao.insert(filme);
+            List<Diretor> lista = diretorDao.findAll();
+            for (Diretor diretor : lista){
+                System.out.println(diretor);
             }
 
+            diretorDao.findAll();
+            diretorDao.deleteById(2);
+            System.out.println();
+
             conn.commit();
+
             long fim = System.currentTimeMillis();
             long total = fim - inicio;
             System.out.println("Tempo com transação: " + total / 1000.0 + " segundos");
